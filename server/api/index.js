2d3d8b9389b9695789ecc998b5487802ac7242fa
Ankan Mandal <ankanmandal2001@gmail.com>
2025-07-12 14:18:01 +0530

    new configurations of vercel added

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
-const app=require('./app.js')
-const logger=require('./utils/Logger.js')
+const app=require('../app.js')
+const logger=require('../utils/Logger.js')
 
 const port=process.env.PORT || 3000
 
